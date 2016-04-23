03e8123cf499a99f73e5366a599ecabaeaeb0714
JChanda <jmcnarf@aol.com>
2016-04-23 15:54:11 -0400

    Added files via upload

---
+++ CoffeeMakerTest.java
@@ -0,0 +1,432 @@
+package edu.towson.cis.cosc603.project5.coffeemaker;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+/**
+ * The class <code>CoffeeMakerTest</code> contains tests for the class 
+ */
+public class CoffeeMakerTest {
+
+	@Test
+	public void testCoffeeMaker_1()
+		throws Exception {
+
+		CoffeeMaker result = new CoffeeMaker();
+
+		// add additional test code here
+		assertNotNull(result);
+	}
+
+	@Test
+	public void testCoffeeMaker_2()
+		throws Exception {
+
+		CoffeeMaker result = new CoffeeMaker();
+
+		// add additional test code here
+		assertNotNull(result);
+	}
+
+	@Test
+	public void testAddInventory_1()
+		throws Exception {
+		CoffeeMaker fixture = new CoffeeMaker();
+		int amtCoffee = 1;
+		int amtMilk = 1;
+		int amtSugar = 0;
+		int amtChocolate = -1;
+
+		boolean result = fixture.addInventory(amtCoffee, amtMilk, amtSugar, amtChocolate);
+
+		// add additional test code here
+		assertEquals(false, result);
+	}
+
+	@Test
+	public void testAddInventory_2()
+		throws Exception {
+		CoffeeMaker fixture = new CoffeeMaker();
+		int amtCoffee = 1;
+		int amtMilk = 1;
+		int amtSugar = 0;
+		int amtChocolate = 1;
+
+		boolean result = fixture.addInventory(amtCoffee, amtMilk, amtSugar, amtChocolate);
+
+		// add additional test code here
+		assertEquals(true, result);
+	}
+
+	@Test
+	public void testAddInventory_3()
+		throws Exception {
+		CoffeeMaker fixture = new CoffeeMaker();
+		int amtCoffee = -1;
+		int amtMilk = 1;
+		int amtSugar = 1;
+		int amtChocolate = 1;
+
+		boolean result = fixture.addInventory(amtCoffee, amtMilk, amtSugar, amtChocolate);
+
+		// add additional test code here
+		assertEquals(false, result);
+	}
+
+	@Test
+	public void testAddInventory_4()
+		throws Exception {
+		CoffeeMaker fixture = new CoffeeMaker();
+		int amtCoffee = 1;
+		int amtMilk = -1;
+		int amtSugar = 1;
+		int amtChocolate = 1;
+
+		boolean result = fixture.addInventory(amtCoffee, amtMilk, amtSugar, amtChocolate);
+
+		// add additional test code here
+		assertEquals(false, result);
+	}
+
+	@Test
+	public void testAddInventory_5()
+		throws Exception {
+		CoffeeMaker fixture = new CoffeeMaker();
+		int amtCoffee = 1;
+		int amtMilk = 1;
+		int amtSugar = 1;
+		int amtChocolate = 1;
+
+		boolean result = fixture.addInventory(amtCoffee, amtMilk, amtSugar, amtChocolate);
+
+		// add additional test code here
+		assertEquals(false, result);
+	}
+
+	@Test
+	public void testAddRecipe_1()
+		throws Exception {
+		CoffeeMaker fixture = new CoffeeMaker();
+		Recipe r = new Recipe();
+
+		boolean result = fixture.addRecipe(r);
+
+		// add additional test code here
+		assertEquals(true, result);
+	}
+
+	@Test
+	public void testAddRecipe_2()
+		throws Exception {
+		CoffeeMaker fixture = new CoffeeMaker();
+		Recipe r = new Recipe();
+
+		boolean result = fixture.addRecipe(r);
+
+		// add additional test code here
+		assertEquals(true, result);
+	}
+
+	@Test
+	public void testAddRecipe_3()
+		throws Exception {
+		CoffeeMaker fixture = new CoffeeMaker();
+		Recipe r = new Recipe();
+
+		boolean result = fixture.addRecipe(r);
+
+		// add additional test code here
+		assertEquals(true, result);
+	}
+
+	@Test
+	public void testCheckInventory_1()
+		throws Exception {
+		CoffeeMaker fixture = new CoffeeMaker();
+
+		Inventory result = fixture.checkInventory();
+
+		// add additional test code here
+		assertNotNull(result);
+		assertEquals("Coffee: 15\nMilk: 15\nSugar: 15\nChocolate: 15\n", result.toString());
+		assertEquals(15, result.getCoffee());
+		assertEquals(15, result.getChocolate());
+		assertEquals(15, result.getMilk());
+		assertEquals(15, result.getSugar());
+	}
+
+	@Test
+	public void testDeleteRecipe_1()
+		throws Exception {
+		CoffeeMaker fixture = new CoffeeMaker();
+		Recipe r = new Recipe();
+
+		boolean result = fixture.deleteRecipe(r);
+
+		// add additional test code here
+		assertEquals(false, result);
+	}
+
+	@Test
+	public void testDeleteRecipe_2()
+		throws Exception {
+		CoffeeMaker fixture = new CoffeeMaker();
+		Recipe r = new Recipe();
+
+		boolean result = fixture.deleteRecipe(r);
+
+		// add additional test code here
+		assertEquals(false, result);
+	}
+
+	@Test
+	public void testDeleteRecipe_3()
+		throws Exception {
+		CoffeeMaker fixture = new CoffeeMaker();
+		Recipe r = new Recipe();
+
+		boolean result = fixture.deleteRecipe(r);
+
+		// add additional test code here
+		assertEquals(false, result);
+	}
+
+	@Test
+	public void testDeleteRecipe_4()
+		throws Exception {
+		CoffeeMaker fixture = new CoffeeMaker();
+		Recipe r = null;
+
+		boolean result = fixture.deleteRecipe(r);
+
+		// add additional test code here
+		assertEquals(false, result);
+	}
+
+	@Test
+	public void testEditRecipe_1()
+		throws Exception {
+		CoffeeMaker fixture = new CoffeeMaker();
+		Recipe oldRecipe = new Recipe();
+		Recipe newRecipe = new Recipe();
+
+		boolean result = fixture.editRecipe(oldRecipe, newRecipe);
+
+		// add additional test code here
+		assertEquals(false, result);
+	}
+
+	@Test
+	public void testEditRecipe_2()
+		throws Exception {
+		CoffeeMaker fixture = new CoffeeMaker();
+		Recipe oldRecipe = new Recipe();
+		Recipe newRecipe = new Recipe();
+
+		boolean result = fixture.editRecipe(oldRecipe, newRecipe);
+
+		// add additional test code here
+		assertEquals(false, result);
+	}
+
+	@Test
+	public void testEditRecipe_3()
+		throws Exception {
+		CoffeeMaker fixture = new CoffeeMaker();
+		Recipe oldRecipe = new Recipe();
+		Recipe newRecipe = new Recipe();
+
+		boolean result = fixture.editRecipe(oldRecipe, newRecipe);
+
+		// add additional test code here
+		assertEquals(false, result);
+	}
+
+	@Test
+	public void testEditRecipe_4()
+		throws Exception {
+		CoffeeMaker fixture = new CoffeeMaker();
+		Recipe oldRecipe = new Recipe();
+		Recipe newRecipe = new Recipe();
+
+		boolean result = fixture.editRecipe(oldRecipe, newRecipe);
+
+		// add additional test code here
+		assertEquals(false, result);
+	}
+
+	@Test
+	public void testEditRecipe_5()
+		throws Exception {
+		CoffeeMaker fixture = new CoffeeMaker();
+		Recipe oldRecipe = new Recipe();
+		Recipe newRecipe = new Recipe();
+
+		boolean result = fixture.editRecipe(oldRecipe, newRecipe);
+
+		// add additional test code here
+		assertEquals(false, result);
+	}
+
+	@Test
+	public void testGetRecipeForName_1()
+		throws Exception {
+		CoffeeMaker fixture = new CoffeeMaker();
+		String name = "";
+
+		Recipe result = fixture.getRecipeForName(name);
+
+		// add additional test code here
+		assertNotNull(result);
+		assertEquals(null, result.toString());
+		assertEquals(null, result.getName());
+		assertEquals(0, result.getAmtMilk());
+		assertEquals(0, result.getAmtChocolate());
+		assertEquals(0, result.getAmtCoffee());
+		assertEquals(0, result.getAmtSugar());
+		assertEquals(0, result.getPrice());
+	}
+
+	@Test
+	public void testGetRecipeForName_2()
+		throws Exception {
+		CoffeeMaker fixture = new CoffeeMaker();
+		String name = "";
+
+		Recipe result = fixture.getRecipeForName(name);
+
+		// add additional test code here
+		assertNotNull(result);
+		assertEquals(null, result.toString());
+		assertEquals(null, result.getName());
+		assertEquals(0, result.getAmtMilk());
+		assertEquals(0, result.getAmtChocolate());
+		assertEquals(0, result.getAmtCoffee());
+		assertEquals(0, result.getAmtSugar());
+		assertEquals(0, result.getPrice());
+	}
+
+	@Test
+	public void testGetRecipeForName_3()
+		throws Exception {
+		CoffeeMaker fixture = new CoffeeMaker();
+		String name = "";
+
+		Recipe result = fixture.getRecipeForName(name);
+
+		// add additional test code here
+		assertNotNull(result);
+		assertEquals(null, result.toString());
+		assertEquals(null, result.getName());
+		assertEquals(0, result.getAmtMilk());
+		assertEquals(0, result.getAmtChocolate());
+		assertEquals(0, result.getAmtCoffee());
+		assertEquals(0, result.getAmtSugar());
+		assertEquals(0, result.getPrice());
+	}
+
+	@Test
+	public void testGetRecipeForName_4()
+		throws Exception {
+		CoffeeMaker fixture = new CoffeeMaker();
+		String name = "";
+
+		Recipe result = fixture.getRecipeForName(name);
+
+		// add additional test code here
+		assertNotNull(result);
+		assertEquals(null, result.toString());
+		assertEquals(null, result.getName());
+		assertEquals(0, result.getAmtMilk());
+		assertEquals(0, result.getAmtChocolate());
+		assertEquals(0, result.getAmtCoffee());
+		assertEquals(0, result.getAmtSugar());
+		assertEquals(0, result.getPrice());
+	}
+
+	@Test
+	public void testGetRecipes_1()
+		throws Exception {
+		CoffeeMaker fixture = new CoffeeMaker();
+
+		Recipe[] result = fixture.getRecipes();
+
+		// add additional test code here
+		assertNotNull(result);
+		assertEquals(4, result.length);
+		assertNotNull(result[0]);
+		assertEquals(null, result[0].toString());
+		assertEquals(null, result[0].getName());
+		assertEquals(0, result[0].getAmtMilk());
+		assertEquals(0, result[0].getAmtChocolate());
+		assertEquals(0, result[0].getAmtCoffee());
+		assertEquals(0, result[0].getAmtSugar());
+		assertEquals(0, result[0].getPrice());
+		assertNotNull(result[1]);
+		assertEquals(null, result[1].toString());
+		assertEquals(null, result[1].getName());
+		assertEquals(0, result[1].getAmtMilk());
+		assertEquals(0, result[1].getAmtChocolate());
+		assertEquals(0, result[1].getAmtCoffee());
+		assertEquals(0, result[1].getAmtSugar());
+		assertEquals(0, result[1].getPrice());
+		assertNotNull(result[2]);
+		assertEquals(null, result[2].toString());
+		assertEquals(null, result[2].getName());
+		assertEquals(0, result[2].getAmtMilk());
+		assertEquals(0, result[2].getAmtChocolate());
+		assertEquals(0, result[2].getAmtCoffee());
+		assertEquals(0, result[2].getAmtSugar());
+		assertEquals(0, result[2].getPrice());
+		assertNotNull(result[3]);
+		assertEquals(null, result[3].toString());
+		assertEquals(null, result[3].getName());
+		assertEquals(0, result[3].getAmtMilk());
+		assertEquals(0, result[3].getAmtChocolate());
+		assertEquals(0, result[3].getAmtCoffee());
+		assertEquals(0, result[3].getAmtSugar());
+		assertEquals(0, result[3].getPrice());
+	}
+
+	@Test
+	public void testMakeCoffee_1()
+		throws Exception {
+		CoffeeMaker fixture = new CoffeeMaker();
+		Recipe r = new Recipe();
+		int amtPaid = 1;
+
+		int result = fixture.makeCoffee(r, amtPaid);
+
+		// add additional test code here
+		assertEquals(1, result);
+	}
+
+	@Test
+	public void testMakeCoffee_2()
+		throws Exception {
+		CoffeeMaker fixture = new CoffeeMaker();
+		Recipe r = new Recipe();
+		int amtPaid = 1;
+
+		int result = fixture.makeCoffee(r, amtPaid);
+
+		// add additional test code here
+		assertEquals(1, result);
+	}
+
+	@Before
+	public void setUp()
+		throws Exception {
+		// add additional set up code here
+	}
+
+	@After
+	public void tearDown()
+		throws Exception {
+		// Add additional tear down code here
+	}
+
+	public static void main(String[] args) {
+		new org.junit.runner.JUnitCore().run(CoffeeMakerTest.class);
+	}
+}
